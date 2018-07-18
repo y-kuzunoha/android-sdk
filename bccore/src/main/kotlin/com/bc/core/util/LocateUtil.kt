abc14966eb25b429b158842a25c382435e6ca753
Văn Hiếu <tvhieuit@gmail.com>
2018-07-18 20:58:30 +0700

    - miss file commit

---
+++ bccore/src/main/kotlin/com/bc/core/util/LocateUtil.kt
@@ -0,0 +1,23 @@
+package com.bc.core.util
+
+import android.app.Activity
+import java.util.*
+
+/**
+ * ____________________________________
+ *
+ * Generator: Hieu.TV - tvhieuit@gmail.com
+ * CreatedAt: 4/9/18
+ * ____________________________________
+ */
+
+//object LocateUtil {
+//	fun updateLanguage(activity: Activity, lang : String) {
+//		val locale = Locale(lang)
+//		val resources = activity.resources
+//		val configuration = resources.configuration
+//		configuration.setLocale(locale)
+//		activity.createConfigurationContext(configuration)
+//		activity.recreate()
+//	}
+//}
